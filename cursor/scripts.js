9991a8fa7807a0a9afe62803107b62b0229c2a49
elizabeth <leezeelola@gmail.com>
2024-11-18 14:58:03 +0100

    update: cursor style tweak

--- cursor/scripts.js
+++ cursor/scripts.js
@@ -3,6 +3,7 @@ const boxes = document.querySelectorAll('.box');
 document.addEventListener('mousemove', (event) => {
   const cursorX = event.clientX
   const cursorY = event.clientY
+
   boxes.forEach(box => {
 
     const rect = box.getBoundingClientRect();
@@ -14,7 +15,7 @@ document.addEventListener('mousemove', (event) => {
     const distanceY = elementY - cursorY;
 
     const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-    const maxDistance = 100;
+    const maxDistance = 200;
     const moveDistance = Math.max(0, maxDistance - distance) / maxDistance;
 
     box.style.transform = `translate(${distanceX * moveDistance}px, ${distanceY * moveDistance}px)`
